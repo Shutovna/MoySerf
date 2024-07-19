e78b3b1beced8aed114297c12ab1e2044f1390b3
Nikitos <shutovna1987@gmail.com>
2024-07-19 21:36:28 +0700

    Project structure

---
+++ src/main/java/ru/shutovna/myserf/MySerfApplication.java
@@ -0,0 +1,12 @@
+package ru.shutovna.myserf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySerfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySerfApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/shutovna/myserf/MySerfApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shutovna.myserf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySerfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
